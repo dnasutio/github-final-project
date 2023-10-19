18362bb9e401d69054e1d8ecbf667dba2016a633
Dylan Nasution <64675642+dnasutio@users.noreply.github.com>
2023-10-19 16:35:43 -0400

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -4,7 +4,7 @@
    # Do not use this in production. Sample purpose only.
    # Author: Upkar Lidder (IBM)
    # Additional Authors:
-   # <your GitHub username>
+   # <dnasutio>
    # Input:
    # p, principal amount
    # t, time period in years
